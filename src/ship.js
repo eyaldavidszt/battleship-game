499af6a9d093e05d1a083d5a38db0d185f39c8d9
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-08-26 05:52:12 +0300

    Write isSunk and hit methods for ship object

--- src/ship.js
+++ src/ship.js
@@ -1,1 +1,11 @@
-export default function Ship() {}
+export default function Ship({ length }) {
+  let hits = 0;
+  const hit = () => {
+    hits += 1;
+  };
+  const isSunk = () => {
+    if (hits === length) return true;
+    return false;
+  };
+  return { isSunk, hit };
+}
