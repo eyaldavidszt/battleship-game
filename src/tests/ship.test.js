ae8e14799f8470c72c717070ffa430f3adc30dcd
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-08-26 05:57:49 +0300

    Initialize ship length and inSunk tests

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,14 @@
 import Ship from "../ship";
+
+it("Initializes ship with correct length?", () => {
+  const ship = Ship({ length: 3 });
+  expect(ship.getLength()).toBe(3);
+});
+
+it("sinks ship when hits = length", () => {
+  const ship = Ship({ length: 3 });
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
